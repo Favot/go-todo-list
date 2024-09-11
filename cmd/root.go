078926115a36b7dd18d330066b3b31a9151ada42
Favot <70208301+Favot@users.noreply.github.com>
2024-09-11 22:00:08 +0200

    Init project using cobra-cli

---
+++ cmd/root.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "go-todo-list",
+	Short: "A simple todo list CLI application to discover Go",
+	Long: `go-todo-list is a small project created to explore and learn Go programming.
+This CLI application allows you to manage your todo items using simple commands.
+
+Features:
+- Add, list, update, and delete todo items
+- Data is stored and retrieved using CSV files for simplicity
+
+This project uses the Cobra library to create a powerful and user-friendly command-line interface.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-todo-list.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
